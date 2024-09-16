208515ec67c1199a642689123c8a8aa3a916d281
Arief-Karditya <nolifegansaya@gmail.com>
2024-09-16 14:03:42 +0700

    Resource Collection

--- tests/Feature/Resource/CategoryResourceTest.php
+++ tests/Feature/Resource/CategoryResourceTest.php
@@ -30,4 +30,59 @@ class CategoryResourceTest extends TestCase
 
         Log::info(json_encode($category));
     }
+
+    public function testCollectionOfResource(): void
+    {
+        $this->seed([CategorySeeder::class]);
+        $categories = Category::all();
+
+
+        self::get("/api/categories")
+            ->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    [
+                        "id" =>         $categories[0]->id,
+                        "name" =>       $categories[0]->name,
+                        "created_at" => $categories[0]->created_at->toJSON(),
+                        "updated_at" => $categories[0]->updated_at->toJSON(),
+                    ],
+                    [
+                        "id" =>         $categories[1]->id,
+                        "name" =>       $categories[1]->name,
+                        "created_at" => $categories[1]->created_at->toJSON(),
+                        "updated_at" => $categories[1]->updated_at->toJSON(),
+                    ],
+                    [
+                        "id" =>         $categories[2]->id,
+                        "name" =>       $categories[2]->name,
+                        "created_at" => $categories[2]->created_at->toJSON(),
+                        "updated_at" => $categories[2]->updated_at->toJSON(),
+                    ],
+                    [
+                        "id" =>         $categories[3]->id,
+                        "name" =>       $categories[3]->name,
+                        "created_at" => $categories[3]->created_at->toJSON(),
+                        "updated_at" => $categories[3]->updated_at->toJSON(),
+                    ],
+                    [
+                        "id" =>         $categories[4]->id,
+                        "name" =>       $categories[4]->name,
+                        "created_at" => $categories[4]->created_at->toJSON(),
+                        "updated_at" => $categories[4]->updated_at->toJSON(),
+                    ],
+                    [
+                        "id" =>         $categories[5]->id,
+                        "name" =>       $categories[5]->name,
+                        "created_at" => $categories[5]->created_at->toJSON(),
+                        "updated_at" => $categories[5]->updated_at->toJSON(),
+                    ],
+                ],
+            ]);
+
+        $categories->each(function ($category) {
+            self::assertNotNull($category,);
+            Log::info(json_encode($category));
+        });
+    }
 }
